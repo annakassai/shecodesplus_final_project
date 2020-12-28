5ad212d04335f5269284bb1f29f6d4d0a7ec185c
annakassai <74617784+annakassai@users.noreply.github.com>
2020-12-29 00:02:04 +0100

    Linke image API to the weather description

--- src/app.js
+++ src/app.js
@@ -30,19 +30,25 @@ function displayWeather(response) {
     let humidityElement= document.querySelector("#humidity");
     let windspeedElement= document.querySelector("#wind-speed");
     let dateElement=document.querySelector("#current-day-time");
+    let iconElement=document.querySelector("#icon");
    
     temperatureElement.innerHTML= Math.round(response.data.main.temp);
-    cityElement.innerHTML= response.data.main.name;
+    cityElement.innerHTML= response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description; 
     pressureElement.innerHTML = response.data.main.pressure;
     humidityElement.innerHTML=response.data.main.humidity;
     windspeedElement.innerHTML= Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt*1000);
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-   
+
    let apiKey = "a762ee58da3312e3b42c763f03cdad42";
    let units = `metric`;
-   let city = "London";
+   let city = "Sopron";
    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
    let apiUrl = `${apiEndpoint}?q=${city}&units=${units}&appid=${apiKey}`;
    console.log(apiUrl);
